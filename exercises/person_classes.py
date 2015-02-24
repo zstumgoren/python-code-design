e6a90109fbd2e72c4bf357cdef9e9c255c31f1c1
Serdar Tumgoren <zstumgoren@gmail.com>
2015-02-23 16:28:40 -0800

    bunch of clean-ups and fixes
    * Move full_name method from PersonWithInit to PersonWithMethod (good catch by Cezary Podkul)
    * Re-organize for clarity and add more explanatory notes
    * Rename PersonWithInit to PersonWithData

--- exercises/person_classes.py
+++ exercises/person_classes.py
@@ -4,20 +4,39 @@ class Person(object):
 
 
 # Class with a method that takes parameters.
-# Not super useful beyond namespacing.
+# Not super useful beyond namespacing purposes (i.e.
+# grouping the method in a seemingly logical spot; same
+# could be accomplished by placing the function in a person.py,
+# with the overhead of a class)
 class PersonNamespace(object):
 
     def full_name(self, first_name, last_name):
         return first_name + " " + last_name
 
+
+
 # Class with an inheritable method
+# (assumes that first_name and last_name attributes exist on subclass)
+# Main difference to note here is the lock of method params and use of "self"
 class PersonWithMethod(object):
 
     def full_name(self):
         return self.first_name + " " + self.last_name
 
-# Class that inherits a method (full_name) from
-# its parent class (PersonWithMethod)
+
+
+# A subclass with initial data set using __init__, but behavior inherited
+class PersonWithData(PersonWithMethod):
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+
+# Another subclass that inherits a method (full_name) from
+# its parent class (PersonWithMethod), but adds an additional
+# data attribute ("party")
 class CandidateBasic(PersonWithMethod):
 
     def __init__(self, first_name, last_name, party):
@@ -27,26 +46,14 @@ class CandidateBasic(PersonWithMethod):
 
 
 
-# Class with initial data AND behavior
-# We use the data in the method.
-class PersonWithInit(object):
-
-    def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
-
-    def full_name(self):
-        return self.first_name + " " + self.last_name
-
-
 # Class that blends data AND behavior
 # inherited from a parent class (through the use of "super")
 # with its own customized data AND behavor
-class CandidateAdvanced(PersonWithInit):
+class CandidateAdvanced(PersonWithData):
 
     def __init__(self, first_name, last_name, party):
         # The built-in super function allows you to call methods on a parent class of the instance. 
-        # In this case, we're calling __init__ on PersonWithInit in order to add the 
+        # In this case, we're calling __init__ on PersonWithData in order to add the 
         # first_name and last_name attributes to our instance.
 
         # More details here:
@@ -73,8 +80,8 @@ if __name__ == '__main__':
     print
 
     # Inheritance is where classes start to shine.  Look ma, no args!!
-    p3 = PersonWithInit('Jane', 'Smith')
-    print "PersonWithInit.full_name (inherited method): %s" % p3.full_name() # Note we've inherited full_name from PersonWithMethod
+    p3 = PersonWithData('Jane', 'Smith')
+    print "PersonWithData.full_name (inherited method): %s" % p3.full_name() # Note we've inherited full_name from PersonWithMethod
     print
 
     cand = CandidateBasic('Jane', 'Smith', 'IND')
